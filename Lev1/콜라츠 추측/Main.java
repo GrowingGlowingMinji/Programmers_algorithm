fcbbbaffcc974ae23ff1b9e54be49204c8303c4d
geniusMJ <77092783+geniusMJ@users.noreply.github.com>
2021-05-08 15:24:16 +0900

    Update Main.java

--- Lev1/콜라츠 추측/Main.java
+++ Lev1/콜라츠 추측/Main.java
@@ -1,6 +1,30 @@
 class Solution {
-    public int solution(int num) {
+    public int solution(long num) {
         int answer = 0;
+        int cnt =0;
+        
+        while(num!=1) {
+        	if(num%2==0) {
+        		num/=2;
+        	}else {
+        		num*=3;
+        		num+=1;
+        	}
+        	
+        	cnt++; 
+        	
+        	if(cnt==500) {
+        		answer = -1;
+                return answer;
+        	}
+        	
+        }
+        
+        answer=cnt;
+    		
+
+        
+        
         return answer;
     }
 }
